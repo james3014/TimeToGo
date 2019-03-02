f7e63f57e23f9c812a13a543428f0ea2506f92f8
james3014 <grantj3014@gmail.com>
2019-03-02 21:03:30 +0000

    Updated flight model

--- FlightService.Libs/Models/Flight.cs
+++ FlightService.Libs/Models/Flight.cs
@@ -6,35 +6,29 @@ namespace FlightService.Libs.Models
 {
     public class Flight
     {
-        public class Metar
+        public class Scheduled
         {
-            public string Airport { get; set; }
-            public int Time { get; set; }
-            public string Cloud_friendly { get; set; }
-            public int Cloud_altitude { get; set; }
-            public string Cloud_type { get; set; }
-            public string Conditions { get; set; }
-            public double Pressure { get; set; }
-            public int Temp_air { get; set; }
-            public int Temp_dewpoint { get; set; }
-            public int Temp_relhum { get; set; }
-            public int Visibility { get; set; }
-            public string Wind_friendly { get; set; }
-            public int Wind_direction { get; set; }
-            public int Wind_speed { get; set; }
-            public int Wind_speed_gust { get; set; }
-            public string Raw_data { get; set; }
+            public string Ident { get; set; }
+            public string Aircrafttype { get; set; }
+            public int Filed_departuretime { get; set; }
+            public int Estimatedarrivaltime { get; set; }
+            public string Origin { get; set; }
+            public string Destination { get; set; }
+            public string OriginName { get; set; }
+            public string OriginCity { get; set; }
+            public string DestinationName { get; set; }
+            public string DestinationCity { get; set; }
         }
-        
-        public class MetarExResult
+
+        public class ScheduledResult
         {
             public int Next_offset { get; set; }
-            public List<Metar> Metar { get; set; }
+            public List<Scheduled> Scheduled { get; set; }
         }
 
         public class RootObject
         {
-            public MetarExResult MetarExResult { get; set; }
+            public ScheduledResult ScheduledResult { get; set; }
         }
     }
 }
